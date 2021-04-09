03ce485be4bea04bf490202727780faed785eeca
radjan000 <80147496+radjan000@users.noreply.github.com>
2021-04-09 23:29:22 +0200

    v1

--- src/w2/Task.java
+++ src/w2/Task.java
@@ -1,20 +1,26 @@
 package w2;
 
-import java.text.SimpleDateFormat;
-import java.util.Locale;
+import java.time.LocalDateTime;
 
 public class Task {
-        String taskName;
-        String taskDescription;
-        Locale.Category taskCategory;
-        java.util.Date  taskDateTimeStart;
-        SimpleDateFormat taskDateTimeStop;
-    public Task(String taskName, String taskDescription, Locale.Category taskCategory, SimpleDateFormat taskDateTimeStop, java.util.Date  taskDateTimeStart) {
+
+    public enum Category {
+        NEW, IN_PROGRESS, DONE
+    }
+
+         String taskName;
+         String taskDescription;
+         Category taskCategory;
+         LocalDateTime taskDateTimeStart;
+         LocalDateTime taskDateTimeStop;
+
+        public Task(String taskName, String taskDescription, Category taskCategory,
+                    LocalDateTime taskDateTimeStop) {
         this.taskName = taskName;
         this.taskDescription = taskDescription;
         this.taskCategory = taskCategory;
         this.taskDateTimeStop = taskDateTimeStop;
-        this.taskDateTimeStart = taskDateTimeStart;
+        this.taskDateTimeStart = LocalDateTime.now();
     }
 
 }
