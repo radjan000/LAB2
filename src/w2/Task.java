758b01e754af57fecc671af692e2b7fe49ba9c7a
radjan000 <radjan000@utp.edu.pl>
2021-04-09 14:51:13 +0200

    .

---
+++ src/w2/Task.java
@@ -0,0 +1,9 @@
+package w2;
+
+public class Task {
+
+    private String buty
+    private String taskDescription
+    private String taskCategory
+    private String taskDateTimeStart
+    private String taskDateTimeStop
